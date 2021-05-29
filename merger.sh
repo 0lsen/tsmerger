e937097aaf60de01aae3456574457bb7573b9231
oLsen <0lsen@users.noreply.github.com>
2021-05-29 11:54:01 +0000

    Create merger.sh

---
+++ merger.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# this will download https://example.com/video/myvideo_X_something.ts with incremental X to file video.ts
+
+BASE_URL=https://example.com/video/
+FILE_NAME_BEFORE=myvideo_
+FILE_NAME_AFTER=_something.ts
+
+FILE_NAME=video.ts
+
+echo -e "\ndownloading segments..."
+I=0
+while wget -q ${BASE_URL}${FILE_NAME_BEFORE}${I}${FILE_NAME_AFTER}
+do
+        cat ${FILE_NAME_BEFORE}${I}${FILE_NAME_AFTER} >> $FILE_NAME
+        rm ${FILE_NAME_BEFORE}${I}${FILE_NAME_AFTER}
+        echo "  segment $I downloaded, merged and deleted"
+        I=$(($I+1))
+done
+
+echo -e "\ndone."
